993bac8536687d16bf9bf9580ba78acc3ce912c0
Adèle <aguessya@yahoo.fr>
2022-07-20 09:16:41 +0200

    TS2Java

---
+++ Photo.java
@@ -0,0 +1,31 @@
+public class Photo {
+
+    private String url;
+    private String filename;
+    private int width;
+    private int number;
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getFilename() {
+        return filename;
+    }
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
